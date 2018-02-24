8c5dec4eec46f51dbad7270b78a1e9e9d36606c4
paulocsilvajr <pauluscave@gmail.com>
2018-02-24 15:38:40 -0300

    Melhorado script jupyter_in_docker

--- exec/jupyter_in_docker.sh
+++ exec/jupyter_in_docker.sh
@@ -13,6 +13,26 @@ NOME_IMAGEM="paulocsilvajr/miniconda3"
 # foi criado um Dockerfile para o miniconda3 que instala o jupyter na sua construção
 # docker run -i -t -p 8888:8888 continuumio/miniconda3 /bin/bash -c "/opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser"
 
+# tratamento de parâmetros
+if [ "$1" == "-d" ]; then
+    D=$1
+    echo "Executando container em backgroung(detack)"
+    echo "Use: docker ps => lista conteiners em execução"
+    echo "     docker logs $NOME_CONTAINER => visualiza o log do container informado"
+    echo "     docker stop $NOME_CONTAINER => para o container informado"
+    echo -e "\n"
+elif [ "$1" == "-h" ]; then
+    echo "Cria container baseado no continuumio/miniconda3 com Jupyter Notebook"
+    echo "Sintaxe: $0 [ -d | -h ]"
+    echo "         -d : [Opcional] Container em background, use 'docker logs $NOME_CONTAINER' para visualizar o endereço/token de acesso."
+    echo "         -h : [Opcional] Exibe esta ajuda."
+
+    exit 1
+else
+    D=""
+fi
+
+
 # Gera a imagem caso não exista
 if [ "$(docker images | grep '\<paulocsilvajr/miniconda3\>')" == "" ]; then
     echo "Criando imagem $NOME_IMAGEM baseado no arquivo miniconda3.dockerfile"
@@ -32,16 +52,5 @@ fi
 echo -e "\nUtilize o endereço/link informado abaixo para acessar o Jupyter Notebook"
 echo "CTRL+c para PARAR"
 echo -e "Volume criado em: $VOLUME\n\n"
-
-if [ "$1" == "-d" ]; then
-    D=$1
-    echo "Executando container em backgroung(detack)"
-    echo "Use: docker ps => lista conteiners em execução"
-    echo "     docker logs $NOME_CONTAINER => visualiza o log do container informado"
-    echo "     docker stop $NOME_CONTAINER => para o container informado"
-    echo -e "\n\n"
-else
-    D=""
-fi
 
 docker run -it $D --rm --name $NOME_CONTAINER -p $PORTA:8888 -v "$VOLUME":/opt/notebooks $USUARIO/miniconda3 /bin/bash -c "/opt/conda/bin/jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=8888 --no-browser --allow-root"
