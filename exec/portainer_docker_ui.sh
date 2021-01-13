2ae17073b7483914e96db9883d66ec5a3061ddc5
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2021-01-13 08:36:26 -0300

    Adicionado script para instalar o Portainer.io, uma UI Web para o Docker

---
+++ exec/portainer_docker_ui.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#Fonte: https://www.cloudsavvyit.com/8911/how-to-get-started-with-portainer-a-web-ui-for-docker/
+docker volume create portainer_data
+docker run -d -p 9000:9000 --name=portainer --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+firefox 127.0.0.1:9000
