59eaa6a04597947e399c28d6dac6e65f53f5639f
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2021-03-02 12:22:23 -0300

    Adicionado senha em script exec/mysql.sh

--- exec/mysql.sh
+++ exec/mysql.sh
@@ -3,12 +3,14 @@
 USER=admin
 PASSWORD=admin
 PORT=3306
+ROOTPW=masterkey
 
-docker run -d -p $PORT:3306 --name mysql-container -v MySQLDB:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=masterkey -e MYSQL_USER=$USER -e MYSQL_PASSWORD=$PASSWORD mysql:5.7 &&
+docker run -d -p $PORT:3306 --name mysql-container -v MySQLDB:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$ROOTPW -e MYSQL_USER=$USER -e MYSQL_PASSWORD=$PASSWORD mysql:5.7 &&
     echo "Configurações do Container MySQL:"
     echo "Porta: $PORT"
     echo "Usuário: $USER"
     echo "Senha: $PASSWORD"
+    echo "Senha root: $ROOTPW"
     echo "Criado volume MySQLDB, detalhes com comando abaixo:"
     echo "docker volume inspect MySQLDB;"
 
