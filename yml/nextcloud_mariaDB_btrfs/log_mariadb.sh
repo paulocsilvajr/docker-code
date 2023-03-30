ee062bb4736196a0ce6bd0bcf1a8cda848e66df8
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-03-30 09:07:48 -0300

    Adicionado script ps.sh para visualizar estado de serviços e refatorado scripts log_...

--- yml/nextcloud_mariaDB_btrfs/log_mariadb.sh
+++ yml/nextcloud_mariaDB_btrfs/log_mariadb.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-watch -n 1 -c 'docker logs --tail 30 $(docker-compose ps -q db)'
+# watch -n 1 -c 'docker-compose logs --tail 30 db'
+docker-compose logs --tail 30 -f db
--- yml/nextcloud_mariaDB_btrfs/log_nextcloud.sh
+++ yml/nextcloud_mariaDB_btrfs/log_nextcloud.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-watch -n 1 -c 'docker logs --tail 30 $(docker-compose ps -q app)'
+# watch -n 1 -c 'docker-compose logs --tail 30 app'
+docker-compose logs --tail 30 -f app
