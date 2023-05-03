308987c3a3bbb784e8ba5626b341492fc98bd604
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-05-03 09:26:58 -0300

    Adicionado mensagens para exibir durante a execução de script gerar_servico.sh

--- yml/nextcloud_mariaDB_btrfs/gerar_servico.sh
+++ yml/nextcloud_mariaDB_btrfs/gerar_servico.sh
@@ -7,8 +7,11 @@ SERVICE_FOLDER=/etc/systemd/system/
 UP=$ABS_BASE/nextcloud-btrfs_up.sh
 DOWN=$ABS_BASE/nextcloud-btrfs_down.sh
 
+echo "Gerando scripts '$UP' e '$DOWN' usados pelo serviço 'nextcloud-btrfs.service'..."
 cp -vi $ABS_BASE/up.sh $UP
 cp -vi $ABS_BASE/down.sh $DOWN
+
+echo "Gerando service 'nextcloud-btrfs.service' baseado em arquivo base 'nextcloud-btrfs.service.base'..."
 cp -v $ABS_BASE/nextcloud-btrfs.service.base $ABS_BASE/nextcloud-btrfs.service
 
 ESCAPED_REPLACE_START=$(printf '%s\n' "$UP" | sed -e 's/[\/&]/\\&/g')
@@ -17,11 +20,13 @@ ESCAPED_REPLACE_STOP=$(printf '%s\n' "$DOWN" | sed -e 's/[\/&]/\\&/g')
 sed -i "s/\$SCRIPTINICIO/$ESCAPED_REPLACE_START/g" $BASE/nextcloud-btrfs.service
 sed -i "s/\$SCRIPTFIM/$ESCAPED_REPLACE_STOP/g" $BASE/nextcloud-btrfs.service
 
-sudo cp -vi $BASE/nextcloud-btrfs.service $SERVICE_FOLDER
+sudo cp -vi $BASE/nextcloud-btrfs.service $SERVICE_FOLDER &&
+    echo "Copiado arquivo do serviço 'nextcloud-btrfs.service' para '$SERVICE_FOLDER'"
 
-sudo systemctl start nextcloud-btrfs
+echo "Iniciando e ativando serviço 'nextcloud-btrfs'..."
+sudo systemctl start nextcloud-btrfs.service
 
-sudo systemctl enable nextcloud-btrfs
+sudo systemctl enable nextcloud-btrfs.service
 
 echo
 echo "sudo systemctl daemon-reload  # para recarregar o daemon"
