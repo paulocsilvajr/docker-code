d443062392ef4dcb5bdd8fc41b97e8d4cecdaaef
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-05-03 09:59:32 -0300

    Adicionado variável para o nome do serviço no script remover_servico.sh e feito substituição no código

--- yml/nextcloud_mariaDB_btrfs/remover_servico.sh
+++ yml/nextcloud_mariaDB_btrfs/remover_servico.sh
@@ -3,12 +3,13 @@
 # fonte: https://superuser.com/questions/513159/how-to-remove-systemd-services
 
 SERVICE_FOLDER=/etc/systemd/system
+SERVICE_NAME=nextcloud-btrfs.service
 
-sudo systemctl stop nextcloud-btrfs.service
+sudo systemctl stop $SERVICE_NAME
 
-sudo systemctl disable nextcloud-btrfs.service
+sudo systemctl disable $SERVICE_NAME
 
-sudo rm $SERVICE_FOLDER/nextcloud-btrfs.service
+sudo rm $SERVICE_FOLDER/$SERVICE_NAME
 
 sudo systemctl daemon-reload
 sudo systemctl reset-failed
