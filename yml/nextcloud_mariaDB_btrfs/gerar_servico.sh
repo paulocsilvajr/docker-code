9694915afaac9693cc0d1b22dfc659d17ec41563
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-05-03 09:58:43 -0300

    Adicionado variável para o nome do serviço no script gerar_servico.sh e feito substituição no código

--- yml/nextcloud_mariaDB_btrfs/gerar_servico.sh
+++ yml/nextcloud_mariaDB_btrfs/gerar_servico.sh
@@ -3,34 +3,35 @@
 BASE=$(dirname $0)
 ABS_BASE=$(dirname $(readlink -f $0))
 SERVICE_FOLDER=/etc/systemd/system/
+SERVICE_NAME=nextcloud-btrfs.service
 
 UP=$ABS_BASE/nextcloud-btrfs_up.sh
 DOWN=$ABS_BASE/nextcloud-btrfs_down.sh
 
-echo "Gerando scripts '$UP' e '$DOWN' usados pelo serviço 'nextcloud-btrfs.service'..."
+echo "Gerando scripts '$UP' e '$DOWN' usados pelo serviço '$SERVICE_NAME'..."
 cp -vi $ABS_BASE/up.sh $UP
 cp -vi $ABS_BASE/down.sh $DOWN
 
-echo "Gerando service 'nextcloud-btrfs.service' baseado em arquivo base 'nextcloud-btrfs.service.base'..."
-cp -v $ABS_BASE/nextcloud-btrfs.service.base $ABS_BASE/nextcloud-btrfs.service
+echo "Gerando service '$SERVICE_NAME' baseado em arquivo base '$SERVICE_NAME.base'..."
+cp -v $ABS_BASE/$SERVICE_NAME.base $ABS_BASE/$SERVICE_NAME
 
 ESCAPED_REPLACE_START=$(printf '%s\n' "$UP" | sed -e 's/[\/&]/\\&/g')
 ESCAPED_REPLACE_STOP=$(printf '%s\n' "$DOWN" | sed -e 's/[\/&]/\\&/g')
 
-sed -i "s/\$SCRIPTINICIO/$ESCAPED_REPLACE_START/g" $BASE/nextcloud-btrfs.service
-sed -i "s/\$SCRIPTFIM/$ESCAPED_REPLACE_STOP/g" $BASE/nextcloud-btrfs.service
+sed -i "s/\$SCRIPTINICIO/$ESCAPED_REPLACE_START/g" $BASE/$SERVICE_NAME
+sed -i "s/\$SCRIPTFIM/$ESCAPED_REPLACE_STOP/g" $BASE/$SERVICE_NAME
 
-sudo cp -vi $BASE/nextcloud-btrfs.service $SERVICE_FOLDER &&
-    echo "Copiado arquivo do serviço 'nextcloud-btrfs.service' para '$SERVICE_FOLDER'"
+sudo cp -vi $BASE/$SERVICE_NAME $SERVICE_FOLDER &&
+    echo "Copiado arquivo do serviço '$SERVICE_NAME' para '$SERVICE_FOLDER'"
 
-echo "Iniciando e ativando serviço 'nextcloud-btrfs'..."
-sudo systemctl start nextcloud-btrfs.service
+echo "Iniciando e ativando serviço '$SERVICE_NAME'..."
+sudo systemctl start $SERVICE_NAME
 
-sudo systemctl enable nextcloud-btrfs.service
+sudo systemctl enable $SERVICE_NAME
 
 echo
 echo "sudo systemctl daemon-reload  # para recarregar o daemon"
-echo "sudo systemctl status nextcloud-btrfs.service  # para status do serviço"
-echo "sudo systemctl start nextcloud-btrfs.service  # para inicializar o serviço"
-echo "sudo systemctl stop nextcloud-btrfs.service  # para parar o serviço"
+echo "sudo systemctl status $SERVICE_NAME  # para status do serviço"
+echo "sudo systemctl start $SERVICE_NAME  # para inicializar o serviço"
+echo "sudo systemctl stop $SERVICE_NAME  # para parar o serviço"
 
