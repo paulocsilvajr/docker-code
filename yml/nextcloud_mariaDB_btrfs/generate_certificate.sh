1a4676b3a0bb77b218c128361e1f4938330179a5
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2023-05-16 16:27:04 -0300

    Renomeado script remover_servico como remove_service. Refatorado script up.sh para verificar existência de certificados. Removido de script generate_certificate o parâmetro subj para exigir que seja passado as informações do host ao criar o certificado

--- yml/nextcloud_mariaDB_btrfs/generate_certificate.sh
+++ yml/nextcloud_mariaDB_btrfs/generate_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-openssl req -subj '/CN=localhost' -x509 -newkey rsa:4096 -nodes -keyout ./conf.d/key.pem -out conf.d/cert.pem -days 365
+openssl req -x509 -newkey rsa:4096 -nodes -keyout ./conf.d/key.pem -out conf.d/cert.pem -days 365
--- yml/nextcloud_mariaDB_btrfs/up.sh
+++ yml/nextcloud_mariaDB_btrfs/up.sh
@@ -2,7 +2,11 @@
 
 BASE=$(dirname $0)
 
-$BASE/mount_volume.sh &&
-    docker-compose -p nextcloud-btrfs -f $BASE/docker-compose.yml up -d &&
-    echo -e "\nDisponível em 'https://localhost:8083'\n"
-
+if [ -f $BASE/conf.d/cert.pem ] && [ -f $BASE/conf.d/key.pem ]; then
+    $BASE/mount_volume.sh &&
+        docker-compose -p nextcloud-btrfs -f $BASE/docker-compose.yml up -d &&
+        echo -e "\nDisponível em 'https://localhost:8083'\n"
+else
+    echo "Certificados inexistentes em '$BASE/conf.d'. Crie os certificados autoassinados com o script 'generate_certificate.sh'"
+    exit 1
+fi
