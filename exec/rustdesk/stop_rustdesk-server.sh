7f0e547bd8aae7c30c7d67aded6c3f6329168266
Paulo C. Silva Jr. <pauluscave@gmail.com>
2024-12-05 15:31:43 -0300

    Adiciona comandos para remover portas abertas do Rustdesk em UFW no script stop_rustdesk-server.sh

--- exec/rustdesk/stop_rustdesk-server.sh
+++ exec/rustdesk/stop_rustdesk-server.sh
@@ -2,3 +2,13 @@
 
 docker container stop hbbs hbbr &&
     docker container prune
+
+echo "Removido portas de Rustdesk em UFW"
+sudo ufw delete allow 21114/tcp
+sudo ufw delete allow 21115/tcp
+sudo ufw delete allow 21116/tcp
+sudo ufw delete allow 21116/udp
+sudo ufw delete allow 21117/tcp
+sudo ufw delete allow 21118/tcp
+sudo ufw delete allow 21119/tcp
+
